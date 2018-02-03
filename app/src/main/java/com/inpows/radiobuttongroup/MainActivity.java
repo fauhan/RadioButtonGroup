a3f5f929024c60df1f802a271d2d8b0d176fb57b
fauhanpugar27 <fauhan.master@gmail.com>
2018-02-04 06:25:09 +0700

    Add Cara2()

--- app/src/main/java/com/inpows/radiobuttongroup/MainActivity.java
+++ app/src/main/java/com/inpows/radiobuttongroup/MainActivity.java
@@ -4,16 +4,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private boolean jawaban = false;
+    RadioButton j1;
+    RadioButton j2;
+    RadioButton j3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final RadioGroup list_opsi = findViewById(R.id.opsi);
+        j1 = findViewById(R.id.j1);
+        j2 = findViewById(R.id.j2);
+        j3 = findViewById(R.id.j3);
+
         list_opsi.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup radioGroup, int id) {
@@ -35,11 +44,8 @@ public class MainActivity extends AppCompatActivity {
         jawab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (jawaban != true) {
-                    Toast.makeText(getApplication(), "Jawaban Kamu Tidak Tepat.", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(getApplication(), "Jawaban Kamu Benar", Toast.LENGTH_SHORT).show();
-                }
+//                Cara1();
+                Cara2();
             }
         });
 
@@ -50,5 +56,27 @@ public class MainActivity extends AppCompatActivity {
                 list_opsi.clearCheck();
             }
         });
+    }
+
+    private void Cara2() {
+        if (j1.isChecked()) {
+            ShowToast("Jawaban Kamu Benar.");
+        } else if (j2.isChecked()) {
+            ShowToast("Jawaban Kamu Tidak Tepat.");
+        } else {
+            ShowToast("Jawaban Kamu Tidak Tepat.");
+        }
+    }
+
+    private void Cara1() {
+        if (!jawaban) {
+            ShowToast("Jawaban Kamu Tidak Tepat.");
+        } else {
+            ShowToast("Jawaban Kamu Benar.");
+        }
+    }
+
+    private void ShowToast(String message) {
+        Toast.makeText(getApplication(), message, Toast.LENGTH_SHORT).show();
     }
 }
